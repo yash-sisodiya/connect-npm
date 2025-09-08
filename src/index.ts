52652b04c420cc5c6aa8a0c1c1dd924c9f9b3f1d
Yash Sisodiya <ysisodiya113@gmail.com>
2025-09-08 20:46:03 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,14 @@
-import "./styles.css";
+import { KryptosConnectProvider } from "./context/KryptosContext";
 import { KryptosConnectButton } from "./molecules/Kryptos-connect";
-import {
-  KryptosConnectProvider,
-  useKryptosConnect,
-} from "./context/KryptosContext";
+import "./styles.css";
 
 // Named exports for module users
-export { KryptosConnectButton, KryptosConnectProvider, useKryptosConnect };
-
-// Default export
-export default {
-  KryptosConnectButton,
-  KryptosConnectProvider,
-  useKryptosConnect,
-};
+export { KryptosConnectButton, KryptosConnectProvider };
 
 // Attach to window if available (UMD/CDN usage)
 if (typeof window !== "undefined") {
   (window as any).KryptosConnect = {
     KryptosConnectButton,
     KryptosConnectProvider,
-    useKryptosConnect,
   };
 }
